48221c040371c9cbf215c5dc30d458d75ceba300
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-04-25 17:08:33 -0500

    title and short description

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,7 @@
 // client/src/components/App.js
-import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
     <BrowserRouter>
@@ -19,7 +11,8 @@ function App() {
             <h1>Test Route</h1>
           </Route>
           <Route path="/">
-            <h1>Page Count: {count}</h1>
+            <h1>Stix Ramen</h1>
+            <p>We are located in Derby, KS off of Rocks Rd! Come on in and taste our delicious bowls of Ramen!</p>
           </Route>
         </Switch>
       </div>
