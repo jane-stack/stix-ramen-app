4e808281eec5a9797c9f74d85c5abb1023bba90e
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-05-09 15:44:28 -0500

    add to cart button under each item

--- client/src/components/MenuCard.js
+++ client/src/components/MenuCard.js
@@ -1,9 +1,14 @@
 function MenuCard({image, name}) {
 
+    const addToCart = () => {
+        console.log("added to cart")
+    }
+
     return (
         <li className="card">
             <img src={image} alt={"ramen name"} />
             <h3>{name}</h3>
+            <button className="btn" onClick={addToCart}>Add to Cart</button>
         </li>
     )
 }
