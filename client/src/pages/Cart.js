b02066bcf16653aa9b24fe916c0f14f3b9f611d0
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-05-11 09:30:20 -0500

    cart tax, delivery fee, and total works.

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,4 +1,9 @@
 function Cart({cartItem, onAddItem, onRemoveItem}) {
+    const itemsPrice = cartItem.reduce((a, c) => a + c.price * c.qty, 0);
+    const taxPrice = itemsPrice * 0.14;
+    const deliveryPrice = 12;
+    const totalPrice = itemsPrice + taxPrice + deliveryPrice;
+
     return (
         <aside className="block col-1">
             <h2>Cart Items</h2>
@@ -17,6 +22,27 @@ function Cart({cartItem, onAddItem, onRemoveItem}) {
                     </div>
                 </div>
             ))}
+            {cartItem.length !== 0 && (
+                <>
+                <hr/>
+                <div className="row">
+                    <div className="col-2">Items</div>
+                    <div className="col-1 text-right">${Number(itemsPrice.toFixed(2))}</div>
+                </div>
+                <div className="row">
+                    <div className="col-2">Tax</div>
+                    <div className="col-1 text-right">${Number(taxPrice.toFixed(2))}</div>
+                </div>
+                <div className="row">
+                    <div className="col-2">Delivery</div>
+                    <div className="col-1 text-right">${Number(deliveryPrice.toFixed(2))}</div>
+                </div>
+                <div className="row">
+                    <div className="col-2"><strong>Total</strong></div>
+                    <div className="col-1 text-right"><strong>${Number(totalPrice.toFixed(2))}</strong></div>
+                </div>
+                </>
+            )}
         </aside>
     )
 }
